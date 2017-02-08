6ea63a585ef3031718cd040a4e33c5b94d86c098
tjaydkSchool <dennis.michael@jubii.dk>
2017-02-08 16:17:39 +0100

    First

---
+++ TriangleTestAsp46/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleTestAsp46.Services;
+
+namespace TriangleTestAsp46
+{
+    class Program
+    {
+        /// <summary>
+        /// Takes three side lenghts and creates a triangle. 
+        /// - If the three sides are alike its a equalsided triangle.
+        /// - If the two sides are alike and the last is shorter its a equallegged triangle
+        /// - If the two sidelenghts combined is longer than the longest its a stumb or pointy triangle
+        /// - other caess should not be accepted !!!
+        /// 
+        /// The inequality rule: c minus b < a < c plus b
+        /// Rules for sidelenghts: c < b < a
+        /// 
+        ///                                         TESTCASES
+        /// =====================================================================================================                                        
+        ///     INPUT           EXP. OUTPUT         (OUTPUT) WAIT WITH THIS             Test type
+        /// ---------------------------------------------------------------------
+        /// |   3, 3, 3     |   Equilateral     |                               |       Correct type of triangle
+        /// ---------------------------------------------------------------------
+        /// |   2, 2, 1     |   Isosceles       |                               |       Correct type of triangle
+        /// ---------------------------------------------------------------------
+        /// |   3, 4, 5     |   Scalene         |                               |       Correct type of triangle
+        /// ---------------------------------------------------------------------
+        /// |   a, b, c     |   invalid input   |                               |       Only int numbers allowed
+        /// ---------------------------------------------------------------------
+        /// |   -1, 3, 3    |   invalid input   |                               |       Only positive values
+        /// ---------------------------------------------------------------------
+        /// |   0, 3, 3     |   invalid input   |                               |       No zero values 
+        /// ---------------------------------------------------------------------
+        /// |   1, 3, 6     |   invalid input   |                               |       Inequality test
+        /// ---------------------------------------------------------------------
+        /// |   max+1, 3, 6 |   invalid input   |                               |       Not an integer value
+        /// ---------------------------------------------------------------------
+        /// =====================================================================================================
+
+        private static int a, b, c;
+        private static Triangle t = new Triangle();
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(
+                    "THE TRIANGLE v. 0.1 \n" +
+                    "This program takes 3 integer values as side lengths and calculates if they make up a triangle, \n" +
+                    "if they do, then it will return the type of triangle created. \n\n" +
+                    "!!NB - Only integer values are accepted.\n" +
+                    "========================================\n" +
+                    "Rules of a triangle:\n" +
+                    "c-b < a < c+b\n" +
+                    "c < b < a\n\n" +
+                    "Press any key to start..."
+                    );
+                Console.ReadKey();
+                Console.Clear();
+
+                Console.WriteLine("Input value a:");
+                a = getInput();
+
+                Console.WriteLine("Input value b:");
+                b = getInput();
+
+                Console.WriteLine("Input value c:");
+                c = getInput();
+
+                Console.Clear();
+                Console.WriteLine("Calculating...");
+                string triangle = t.calculateTriangle(a, b, c);
+                Console.Clear();
+
+                Console.WriteLine(triangle);
+                Console.WriteLine("\nPress Esc to exit, or any key to try again");
+                if (Console.ReadKey().Key == ConsoleKey.Escape) { Environment.Exit(0); }
+            }
+        }
+
+        public static int getInput()
+        {
+            bool takeInput = true;
+            int val = 1;
+            while (takeInput)
+            {
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out val))
+                {
+                    val = int.Parse(input);
+                    takeInput = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, please try again:\n");
+                }
+            }
+            return val;
+        }
+    }
+}
---
+++ TriangleTestAsp46/Services/Triangle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleTestAsp46.Services
+{
+    class Triangle
+    {
+        /// <summary>
+        /// Calculates a triangle given 3 integer inputs
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns>Returns a stirng representation of the triagel type, or 'Invalid inputs' if not compatible</returns>
+        public string calculateTriangle(int a, int b, int c)
+        {
+            if (checkValue(a) && checkValue(b) && checkValue(c))
+            {
+                int[] values = orderedValues(new int[] { a, b, c });
+                if (checkRules(values)) { return calculateType(values); }
+            }
+            return "Invalid inputs";
+        }
+
+        /// <summary>
+        /// Check that values are positive numbers
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        private bool checkValue(int val)
+        {
+            if (val <= 0) { return false; }
+            return true;
+        }
+
+        /// <summary>
+        /// Sort array in ascending order
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        private int[] orderedValues(int[] values)
+        {
+            return values.OrderBy(v => v).ToArray();
+        }
+
+        /// <summary>
+        /// Checks if the values in the array follows the rules for a triangle
+        /// 
+        /// c minus b < a < c plus b
+        /// c < b < a
+        /// </summary>
+        /// <param name="orderedVal"></param>
+        /// <returns></returns>
+        private bool checkRules(int[] orderedVal)
+        {
+            if (orderedVal[0] <= orderedVal[1] && orderedVal[1] <= orderedVal[2])
+            {
+                if ((orderedVal[2] - orderedVal[0]) < orderedVal[1] && orderedVal[1] < (orderedVal[2] + orderedVal[0]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Takes an array of integeres and checks for the type of triangle
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        private string calculateType(int[] values)
+        {
+            if (values[0] == values[1] && values[0] == values[2]) { return "Equilateral triangle"; }
+            if (values[0] == values[1] || values[0] == values[2] || values[1] == values[2]) { return "Isosceles triangle"; }
+            // if none of the above is the case then return Scalene triangle
+            return "Scalene triangle";
+        }
+    }
+}
